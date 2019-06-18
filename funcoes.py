2691af669b89a2dbf6f4ac04f3ba60ca65f899fd
lucasmieri <38170990+lucasmieri@users.noreply.github.com>
2019-06-18 17:26:54 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,27 @@
+
+#%%
+import numpy as np
+from matplotlib.pyplot import*
+from math import *
+from seaborn import *
+
+
+#%%
+def euler_murayama(y0, equation1, ):{
+
+    }
+#%%
+def equation1(σ, θ, μ, dt, t):{
+
+    y=np.zeros(t)
+
+    return y;
+    }
+#%%
+def all_plots():{
+
+}
+#%%
+def erro():{
+    }
+

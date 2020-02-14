af7263a0fcd7a85f447288692825673e8e3fe847
lucasmieri <38170990+lucasmieri@users.noreply.github.com>
2020-02-13 22:03:29 -0300

    Update MaoZang.R

--- R Project/MaoZang.R
+++ R Project/MaoZang.R
@@ -1,7 +1,3 @@
-lamb  = 2
-mu    = 1
-Xzero = 1
-
 ### Definir os parâmetos usando os dados do artigo do Zang
 # TODO
 #lambda_1 = 
@@ -11,12 +7,25 @@ Xzero = 1
 #sigma_1 = 
 #sigma_2 = 
 
+lambda<-c(6.04, 8.90) #alta e baixa
+sigma<-c(0.44, 0.63) 
+prob=0
+r<-c(1.5, -1.61)
+p<- matrix(c(-lambda[1], lambda[1], lambda[2], -lambda[2]), nrow=2)
+Q<-expm(p)
+Xzero = 70.5
+
 ################## PARTE II #########################
 # Fazendo os gráficos dos caminhos
-p = 1
-R=2**p
+
+N= 2**9 # number of discrete time steps
+R=2
 L=N/R
+T=1
+dt = T/N
 Dt=R*dt
+
+
 
 set.seed(15) # Fazendo a semente ser 15 para dar sempre o mesmo caminho
 time = seq(0,T,Dt)
@@ -25,18 +34,42 @@ dW = sqrt(dt)*rnorm(N)
 # Euler-Maruyama:
 XemV = rep(0,floor(L)+1)
 XemV[1]=Xzero
+Xemv_zhang<-XemV
+
+
+#optimal solution
+#dx= rdt+ sigma dw
+
 for(j in 1:floor(L))
 {
+    
     # TODO
     #Gerar dinamica da cadeia de Markov
     # 1) Gerar P (usar expm)
+    
+    
     # 2) Gerar o estado, gerando um número aleatório uniforme em (0,1)
     
     Winc= sum(dW[(R*(j-1)+1):(R*j)])
     
     #Usar os paramatros dependente da cadeia de Markov
     # TODO
-    XemV[j+1] = XemV[j] + Dt*lamb*XemV[j] + mu*XemV[j]*Winc
+    
+    
+    
+    
+   # XemV[j+1] = XemV[j] + Dt*lambda[prob+1]*XemV[j] + sigma[prob+1]*XemV[j]*Winc
+    
+    #zhang
+    
+    Xemv_zhang[j+1] = Xemv_zhang[j] + Dt*r[prob+1]*XemV[j] + sigma[prob+1]*XemV[j]*Winc
+    
+    prob<- sample(0:1, 1)
+    
 }
-plot(time,XtrueV,main="Geometric Brownian Motion\n (Zhang's Model)",
-     ylab="",xlab="Time",type="l",col="red",ylim=c(0,10))
+
+
+
+
+plot(time,Xemv_zhang,main="Geometric Brownian Motion\n (Zhang's Model)",
+     ylab="",xlab="Time",type="l",col="red")

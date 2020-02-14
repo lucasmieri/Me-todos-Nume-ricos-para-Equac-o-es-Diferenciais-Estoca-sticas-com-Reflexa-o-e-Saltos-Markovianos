c17c30ecfbc187b3140aaaafcc6949864c8b9884
Saul Leite <llsaull@gmail.com>
2020-02-14 11:51:21 -0300

    modified:   MaoZang.R 	modified:   Zhang.png 	modified:   Zhang2.png

--- R Project/MaoZang.R
+++ R Project/MaoZang.R
@@ -1,75 +1,51 @@
-### Definir os parâmetos usando os dados do artigo do Zang
-# TODO
-#lambda_1 = 
-#lambda_2 = 
-#mu_1 = 
-#mu_2 = 
-#sigma_1 = 
-#sigma_2 = 
+library(expm)
 
 lambda<-c(6.04, 8.90) #alta e baixa
 sigma<-c(0.44, 0.63) 
 prob=0
 r<-c(1.5, -1.61)
-p<- matrix(c(-lambda[1], lambda[1], lambda[2], -lambda[2]), nrow=2)
-Q<-expm(p)
-Xzero = 70.5
+mu <- r + sigma^2/2
+X0 = 70.5
+p0 = c(0.5,0.5)
 
 ################## PARTE II #########################
 # Fazendo os gráficos dos caminhos
 
-N= 2**9 # number of discrete time steps
+N= 2**12 # number of discrete time steps
 R=2
 L=N/R
 T=1
 dt = T/N
 Dt=R*dt
 
-
-
-set.seed(15) # Fazendo a semente ser 15 para dar sempre o mesmo caminho
+#set.seed(15) # Fazendo a semente ser 15 para dar sempre o mesmo caminho
 time = seq(0,T,Dt)
 dW = sqrt(dt)*rnorm(N)
 
 # Euler-Maruyama:
-XemV = rep(0,floor(L)+1)
-XemV[1]=Xzero
-Xemv_zhang<-XemV
+X   = rep(0,floor(L)+1)
+X[1]= X0
+A   = rep(0,floor(L)+1)
+A[1] = sample(c(1,2),1,prob=p0)
 
-
-#optimal solution
-#dx= rdt+ sigma dw
+# Gerar P (usar expm)
+Rho <- matrix(c(-lambda[1], lambda[1], lambda[2], -lambda[2]), nrow=2,byrow=TRUE)
+P<-expm(Rho*Dt)
 
 for(j in 1:floor(L))
 {
-    
-    # TODO
-    #Gerar dinamica da cadeia de Markov
-    # 1) Gerar P (usar expm)
-    
-    
-    # 2) Gerar o estado, gerando um número aleatório uniforme em (0,1)
-    
+    # Gerar dinamica da cadeia de Markov
+    A[j+1] = sample(c(1,2),1,prob=P[A[j],])
+
     Winc= sum(dW[(R*(j-1)+1):(R*j)])
-    
-    #Usar os paramatros dependente da cadeia de Markov
-    # TODO
-    
-    
-    
-    
-   # XemV[j+1] = XemV[j] + Dt*lambda[prob+1]*XemV[j] + sigma[prob+1]*XemV[j]*Winc
-    
-    #zhang
-    
-    Xemv_zhang[j+1] = Xemv_zhang[j] + Dt*r[prob+1]*Xemv_zhang[j] + sigma[prob+1]*Xemv_zhang[j]*Winc
-    
-    prob<- sample(0:1, 1)
-    
+    X[j+1] = X[j] + Dt*mu[A[j+1]]*X[j] + sigma[A[j+1]]*X[j]*Winc
 }
 
+# https://www.statmethods.net/advgraphs/axes.html
+par(mfrow=c(1,1))
+plot(time,X,main="Geometric Brownian Motion\n (Zhang's Model)",
+     ylab="",xlab="Time",type="l",col="red",lwd=1)
+par(new=TRUE)
+plot(time,2-A,ylab="",xlab="Time",type="s",lty=2,col="blue",lwd=2,axes=FALSE)
+axis(4, at=c(0,1),las=1)
 
-
-
-plot(time,Xemv_zhang,main="Geometric Brownian Motion\n (Zhang's Model)",
-     ylab="",xlab="Time",type="l",col="red")
